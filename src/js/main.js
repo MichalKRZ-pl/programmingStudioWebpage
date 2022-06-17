c05f4fb21605285e8b68f313c946848fffb43a94
Michal Krz <michal.krzywdzinski@outlook.com>
2022-06-17 19:26:12 +0200

    added checking if all form data is input properly

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,36 @@
 //footer related variables
 let $footerYear;
+
 //burger button/mobile menu variables
 let $burgerBtn;
 let $mobileMenu;
+let $allNavItems;
+
+//contact form controls
+let $nameField;
+let $emailField;
+let $phoneField;
+let $companyField;
+let $messageField;
+let $formButton;
+let $isFormValid;
+let $allFormFields;
 
 const prepareDOMElements = () => {
   $footerYear = document.querySelector(".footer__year");
   $burgerBtn = document.querySelector(".header__burger");
   $mobileMenu = document.querySelector(".mobile-menu");
+  $allNavItems = document.querySelectorAll(".mobile-menu__item");
+
+  if (document.querySelector("form") != null) {
+    $nameField = document.querySelector('[name="name"]');
+    $emailField = document.querySelector('[name="email"]');
+    $phoneField = document.querySelector('[name="phone"]');
+    $companyField = document.querySelector('[name="company"]');
+    $messageField = document.querySelector('[name="message"]');
+    $formButton = document.querySelector(".form__button");
+    $allFormFields = document.querySelectorAll(".form__input");
+  }
 };
 
 const handleFooterYear = () => {
@@ -19,9 +42,68 @@ const handleBurgerBtn = () => {
   $mobileMenu.classList.toggle("mobile-menu--active");
 };
 
+const handleNavItems = () => {
+  $allNavItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      $mobileMenu.classList.remove("mobile-menu--active");
+    });
+  });
+};
+
+const showError = (input, msg = "") => {
+  input.classList.add("form__input--error");
+
+  if (msg === "") {
+    input.placeholder = `Wprowadź ${input.previousElementSibling.innerText
+      .slice(0, -1)
+      .toLowerCase()}`;
+  } else {
+    input.placeholder = msg;
+  }
+};
+
+const clearError = (input) => {
+  input.classList.remove("form__input--error");
+};
+
+const checkForm = (input) => {
+  input.forEach((el) => {
+    if (el.value === "") {
+      showError(el);
+      $isFormValid = false;
+    } else {
+      clearError(el);
+    }
+  });
+};
+
+const checkMail = (input) => {
+  const regExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (regExp.test(input.value)) {
+    clearError(input);
+  } else {
+    showError(input, "Wprowadź poprawny adres e-mail");
+    $isFormValid = false;
+  }
+};
+
 const prepareDOMEvents = () => {
   handleFooterYear();
   $burgerBtn.addEventListener("click", handleBurgerBtn);
+  handleNavItems();
+
+  if ($formButton != null) {
+    $formButton.addEventListener("click", (e) => {
+      $isFormValid = true;
+      checkForm([$nameField, $companyField]);
+
+      checkMail($emailField);
+
+      if (!$isFormValid) {
+        e.preventDefault();
+      }
+    });
+  }
 };
 
 const main = () => {
