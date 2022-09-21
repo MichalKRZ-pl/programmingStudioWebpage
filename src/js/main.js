95f6cfdddb39031a18c82de400aa834dcc7e80ce
Michal Krz <michal.krzywdzinski@outlook.com>
2022-09-21 13:54:49 +0200

    initial swiper implementation

--- src/js/main.js
+++ src/js/main.js
@@ -16,6 +16,41 @@ let $formButton;
 let $isFormValid;
 let $allFormFields;
 
+const swiper = new Swiper(".services__swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 const prepareDOMElements = () => {
   $footerYear = document.querySelector(".footer__year");
   $burgerBtn = document.querySelector(".header__burger");
